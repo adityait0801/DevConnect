d60c6659f3e6d492f7ad00edb8d1b5f7d751111e
Aditya Singh <aditya.it0801@gmail.com>
2025-04-01 17:39:58 +0530

    Base code

--- src/app.js
+++ src/app.js
@@ -2,13 +2,24 @@ const express = require('express');
 
 const app = express(); //instance of the express is created here
 
-app.use("/test", (req, res)=> {     //To handle the incoming request we use this. 
+app.use("/test", (req, res)=> {       //To handle the incoming request we use this. 
     res.send("Hello from the Server");  //this function is called request handler function
+})                                  // anything matches after / this route handler will match it. 
+
+app.use("/hello", (req, res)=> {      //To handle the incoming request we use this. 
+    res.send("Hello");              //this function is called request handler function
 })
 
-app.use("/hello", (req, res)=> {     //To handle the incoming request we use this. 
-    res.send("Hello");  //this function is called request handler function
-})
+app.use("/user",
+    (req, res)=> {  
+        console.log("");
+        res.send("Response 1st");   
+    }, 
+    (req, res)=> {
+        console.log("")  
+        res.send("Response 2nd");   
+    }
+)
 
 app.listen(3000, ()=> {
     console.log("Server is running on PORT 3000");
