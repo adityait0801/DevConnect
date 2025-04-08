5479932f447244a0fe62c90c520d3ae88f7672c0
Aditya Singh <aditya.it0801@gmail.com>
2025-04-08 16:44:01 +0530

    Schema validations

--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,46 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName : {
-        type : String
+        type : String,
+        required : true,
     }, 
     lastName : {
-        type : String
+        type : String,
+        required : true,
     },
     age : {
         type : Number
     },
     emailID : {
-        type : String
+        type : String,
+        required : true,
+        unique : true,
+        trim : true,
+        lowercase : true
     },
     password : {
-        type : String
+        type : String,
+        required : true,
+        minLen : 8
     },
     gender : {
-        type : String
+        type : String,
+        validate(value) {
+            if(!["male", "female", "other"].includes(value)) {
+                throw new Error("Gender data is not valid");
+            }
+        }
+    },
+    about : {
+        type : String,
+        default : "This is a default about of the user !"
+    },
+    skills : {
+        type :  [String],
     }
+}, 
+{
+    timestamps : true
 });
 
 const User = mongoose.model("User", userSchema);
